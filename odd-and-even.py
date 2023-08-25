2294428fd848bbefee88e17e7926dab32d64f4c9
Paranjothir <paranjothi502@gmail.com>
2023-08-25 20:33:51 +0530

    py

---
+++ odd-and-even.py
@@ -0,0 +1,16 @@
+#Your code below
+a=input()
+l=len(a)
+out = ""
+f=0
+if l%2==0:
+    for i in range(0,l,2):
+        out+=a[i]+a[i]
+        
+else:
+    for i in range(1,l,2):
+        out+=a[i]+a[i]
+        f+=1
+if f!=0:
+    out+=a[l-1]
+print(out,end=" ")
