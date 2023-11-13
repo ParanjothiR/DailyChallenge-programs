2d7ad892edf556cb4f0f5778900f3bd5f7077193
Paranjothir <paranjothi502@gmail.com>
2023-11-13 21:25:32 +0530

    daily challenge1

---
+++ squareonesides.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Hello {
+
+    public static void main(String[] args) {
+		//Your Code Here
+		Scanner sc=new Scanner(System.in);
+		double k=sc.nextDouble();
+		double t=Math.sqrt(k)/2;
+		System.out.format("%.2f",t);
+
+	}
+}
